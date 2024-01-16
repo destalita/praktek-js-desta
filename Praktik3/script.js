179df5e759c132ddefedd62d6eab55d9e589f4bb
Destalita satya <142203318+destalita@users.noreply.github.com>
2024-01-16 15:23:45 +0800

    Add files via upload

---
+++ Praktik3/script.js
@@ -0,0 +1,7 @@
+const Negara ="Panama";
+const Benua ="Amerika Utara "
+let   Populasi = 4.351;
+const Kepulauan = "False";
+const Bahasa = "Spanyol";
+
+console.log(`Negara  ${Negara}, adalah negara yang berada Di Benua ${Benua}, Dengan populasi sebanyak ${Populasi}, Jiwa, Bahasa Nasional ${Bahasa}`);
